a2c67ef2d98eeff2a99a01f866329101a6e563c2
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-12-02 10:49:59 +0000

    Updates

---
+++ precache-manifest.fa01cec3a499bbce3acd23ea21f40871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3f6e7d0f69b48306e4a7b53d812e35",
+    "url": "/To-Do-List-React/index.html"
+  },
+  {
+    "revision": "1e2b0ee9ac9eacff68b4",
+    "url": "/To-Do-List-React/static/css/main.926045af.chunk.css"
+  },
+  {
+    "revision": "da4bbcc5818ad3a4f6fa",
+    "url": "/To-Do-List-React/static/js/2.c3ee443c.chunk.js"
+  },
+  {
+    "revision": "1e2b0ee9ac9eacff68b4",
+    "url": "/To-Do-List-React/static/js/main.fc5ebf50.chunk.js"
+  },
+  {
+    "revision": "8dfdd691ddd68b0b54e8",
+    "url": "/To-Do-List-React/static/js/runtime-main.2da5accc.js"
+  }
+]);
